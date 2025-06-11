dc179e614042a3a7fd68311ee0c6dd7d8ce5dac8
uughf <uughf@student.kit.edu>
2025-06-11 20:52:14 +0200

    implement while from for and not for from while to fix bug with continue in for

--- src/compiler/frontend/for.clj
+++ src/compiler/frontend/for.clj
@@ -4,15 +4,58 @@
             [compiler.frontend.common.parser :as p]
             [compiler.frontend.statement :as stmt]
             [compiler.frontend.expression :as expr]
-            [compiler.frontend.block]
-            [compiler.frontend.while]
-            [compiler.frontend.block :as block]
-            [compiler.frontend.while :as while]
-            [compiler.frontend.common.error :as err]))
+            [compiler.frontend.common.namespace :as name]
+            [compiler.frontend.bool :as bool]
+            [compiler.frontend.common.type :as type]
+            [compiler.frontend.common.error :as err]
+            [compiler.frontend.common.id :as id]
+            [compiler.middleend.ir :as ir]))
 
 (defn- token [kind]
   (fn [tok]
     (= (::lex/kind tok) kind)))
+
+(def ^:dynamic in-loop false)
+(def ^:dynamic dyn-label-cont nil)
+(def ^:dynamic dyn-label-end nil)
+
+(p/defrule stmt/parse-statement ::break
+  [_ (token ::lex/break)
+   _ (token ::lex/semicolon)]
+  {::ast/kind ::break
+   ::ast/children []})
+
+(defmethod ast/pretty-print ::break [_] "break;")
+(defmethod name/resolve-names-stmt ::break [break env] [break env])
+(defmethod stmt/typecheck ::break [break env] [break env])
+(defmethod ast/check-after-parse ::break [break]
+  (if (not in-loop)
+    (err/add-error break (err/make-semantic-error "break outside of loop"))
+    break))
+(defmethod stmt/to-ir ::break [_] [[::ir/goto dyn-label-end]])
+
+(p/defrule stmt/parse-statement ::continue
+  [_ (token ::lex/continue)
+   _ (token ::lex/semicolon)]
+  {::ast/kind ::continue
+   ::ast/children []})
+
+(defmethod ast/pretty-print ::continue [_] "continue;")
+(defmethod name/resolve-names-stmt ::continue [continue env] [continue env])
+(defmethod stmt/typecheck ::continue [continue env] [continue env])
+(defmethod ast/check-after-parse ::continue [c]
+  (if (not in-loop)
+    (err/add-error c (err/make-semantic-error "continue outside of loop"))
+    c))
+(defmethod stmt/to-ir ::continue [_] [[::ir/goto dyn-label-cont]])
+
+(defn for-node [init test step body]
+  {::ast/kind ::for
+   ::ast/children [::init ::test ::step ::body]
+   ::init init
+   ::test test
+   ::step step
+   ::body body})
 
 (p/defrule stmt/parse-statement ::for
   [_ (token ::lex/for)
@@ -24,9 +67,61 @@
    step (p/maybe stmt/parse-simp)
    _ (token ::lex/right-parentheses)
    body stmt/parse-statement]
-  (block/block-node
-   [(if init init (block/block-node []))
-    (while/while-node test
-                      (block/block-node
-                       [body
-                        (if step step (block/block-node []))]))]))
+  (for-node init test step body))
+
+(defmethod ast/check-after-parse ::for [for]
+  (binding [in-loop true]
+    (assoc for
+           ::test (ast/check-after-parse (::test for))
+           ::body (ast/check-after-parse (::body for)))))
+
+(defmethod ast/pretty-print ::for [for]
+  (str "for (" (if (::init for) (ast/pretty-print (::init for)) "") ";" (ast/pretty-print (::test for)) ";" (if (::step for) (ast/pretty-print (::step for)) "") ")\n{"
+       (ast/pretty-print (::body for))
+       "}\n"))
+
+(defmethod name/resolve-names-stmt ::for [for env]
+  (let [[init new-env] (if (::init for) (name/resolve-names-stmt (::init for) env) (::init for))
+        test (name/resolve-names-expr (::test for) new-env)
+        [step new-env] (if (::step for) (name/resolve-names-stmt (::step for) new-env) (::step for))
+        [body _] (name/resolve-names-stmt (::body for) new-env)
+        for (assoc for
+                     ::init init
+                     ::test test
+                     ::step step
+                     ::body body)]
+    [for env]))
+
+(defmethod stmt/typecheck ::for [for env]
+  (let [[init new-env] (if (::init for) (stmt/typecheck (::init for) env) (::init for))
+        test (expr/typecheck (::test for) new-env)
+        [step new-env] (if (::step for) (stmt/typecheck (::step for) new-env) (::step for))
+        [body _] (stmt/typecheck (::body for) new-env)
+        for (assoc for
+                     ::test test
+                     ::body body)
+        for (if (type/equals (::type/type test) bool/bool-type)
+                for
+                (err/add-error for (err/make-semantic-error (str "type mismatch. Test in loop has type " (::type/type test) " but should have type bool."))))]
+    [for env]))
+
+(defmethod stmt/to-ir ::for [for]
+  (let [test-tmp (id/make-tmp)
+        label-start (id/make-label "start")
+        label-cont (id/make-label "cont")
+        label-end (id/make-label "end")]
+    (binding
+     [dyn-label-cont label-cont
+      dyn-label-end label-end]
+      (into [] (concat
+                (stmt/to-ir (::init for))
+                [[::ir/target label-start]]
+                (expr/to-ir (::test for) test-tmp)
+                [[::ir/if-false-jmp test-tmp label-end]]
+                (stmt/to-ir (::body for))
+                [[::ir/target label-cont]]
+                (stmt/to-ir (::step for))
+                [[::ir/goto label-start]
+                 [::ir/target label-end]])))))
+
+(defmethod stmt/minimal-flow-paths ::for [block] [[]])
--- src/compiler/frontend/while.clj
+++ src/compiler/frontend/while.clj
@@ -9,51 +9,12 @@
             [compiler.frontend.common.type :as type]
             [compiler.frontend.common.error :as err]
             [compiler.frontend.common.id :as id]
-            [compiler.middleend.ir :as ir]))
+            [compiler.frontend.for :as for]
+            [compiler.frontend.block :as block]))
 
 (defn- token [kind]
   (fn [tok]
     (= (::lex/kind tok) kind)))
-
-(def ^:dynamic in-loop false)
-(def ^:dynamic dyn-label-start nil)
-(def ^:dynamic dyn-label-end nil)
-
-(p/defrule stmt/parse-statement ::break
-  [_ (token ::lex/break)
-   _ (token ::lex/semicolon)]
-  {::ast/kind ::break
-   ::ast/children []})
-
-(defmethod ast/pretty-print ::break [_] "break;")
-(defmethod name/resolve-names-stmt ::break [break env] [break env])
-(defmethod stmt/typecheck ::break [break env] [break env])
-(defmethod ast/check-after-parse ::break [break]
-  (if (not in-loop)
-    (err/add-error break (err/make-semantic-error "break outside of loop"))
-    break))
-(defmethod stmt/to-ir ::break [_] [[::ir/goto dyn-label-end]])
-
-(p/defrule stmt/parse-statement ::continue
-  [_ (token ::lex/continue)
-   _ (token ::lex/semicolon)]
-  {::ast/kind ::continue
-   ::ast/children []})
-
-(defmethod ast/pretty-print ::continue [_] "continue;")
-(defmethod name/resolve-names-stmt ::continue [continue env] [continue env])
-(defmethod stmt/typecheck ::continue [continue env] [continue env])
-(defmethod ast/check-after-parse ::continue [c]
-  (if (not in-loop)
-    (err/add-error c (err/make-semantic-error "continue outside of loop"))
-    c))
-(defmethod stmt/to-ir ::continue [_] [[::ir/goto dyn-label-start]])
-
-(defn while-node [test body]
-  {::ast/kind ::while
-   ::ast/children [::test ::body]
-   ::test test
-   ::body body})
 
 (p/defrule stmt/parse-statement ::while
   [_ (token ::lex/while)
@@ -61,51 +22,4 @@
    test expr/parse-expr
    _ (token ::lex/right-parentheses)
    body stmt/parse-statement]
-  (while-node test body))
-
-(defmethod ast/check-after-parse ::while [while]
-  (binding [in-loop true]
-    (assoc while 
-           ::test (ast/check-after-parse (::test while))
-           ::body (ast/check-after-parse (::body while)))))
-
-(defmethod ast/pretty-print ::while [while]
-  (str "while (" (ast/pretty-print (::test while)) ")\n{"
-       (ast/pretty-print (::body while))
-       "}\n"))
-
-(defmethod name/resolve-names-stmt ::while [while env]
-  (let [test (name/resolve-names-expr (::test while) env)
-        [body _] (name/resolve-names-stmt (::body while) env)
-        while (assoc while
-                     ::test test
-                     ::body body)]
-    [while env]))
-
-(defmethod stmt/typecheck ::while [while env]
-  (let [test (expr/typecheck (::test while) env)
-        [body _] (stmt/typecheck (::body while) env)
-        while (assoc while
-                     ::test test
-                     ::body body)
-        while (if (type/equals (::type/type test) bool/bool-type)
-                while
-                (err/add-error while (err/make-semantic-error (str "type mismatch. Test in while loop has type " (::type/type test) " but should have type bool."))))]
-    [while env]))
-
-(defmethod stmt/to-ir ::while [while]
-  (let [test-tmp (id/make-tmp)
-        label-start (id/make-label "start")
-        label-end (id/make-label "end")]
-    (binding
-     [dyn-label-start label-start
-      dyn-label-end label-end]
-     (into [] (concat
-               [[::ir/target label-start]]
-               (expr/to-ir (::test while) test-tmp)
-               [[::ir/if-false-jmp test-tmp label-end]]
-               (stmt/to-ir (::body while))
-               [[::ir/goto label-start]
-                [::ir/target label-end]])))))
-
-(defmethod stmt/minimal-flow-paths ::while [block] [[]])
+  (for/for-node nil test nil body))
